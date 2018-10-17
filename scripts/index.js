cf030a75f1c34cd0e496e580c7ba7f943925eb35
Chris Aquino <chris.aquino@gmail.com>
2018-10-17 13:44:48 -0400

    makes ajax requests in parallel with Promise.all

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,14 @@ let cachedJoke = 'Why did chuck norris cross the road? The road knew better than
 let jokePromise;
 
 // function that gets a chuck norris joke
-function getJoke() {
-    return fetch('https://api.icndb.com/jokes/random')
+// function getJoke() {
+function makeAjaxRequest(url) { 
+    return fetch(url)
         .then(convertToJson)
-        .then(cacheJoke)
-        .then(extractJokeText)
-        .catch(showCachedJoke)
-        .then(drawJoke)
+        // .then(cacheJoke)
+        // .then(extractJokeText)
+        // .catch(showCachedJoke)
+        // .then(drawJoke)
     // .then( j => {
     // //   document.body.textContent = j.value.joke
     //     drawJoke(j.value.joke);
@@ -61,11 +62,45 @@ function drawJoke(jokeText) {
 }
 
 
+let URLs = [
+    'https://api.icndb.com/jokes/random',
+    // 'https://zodiacal.herokuapp.com/api',
+    'https://anapioficeandfire.com/api/characters/583',
+    // 'https://anapioficeandfire.com/api/characters/584',
+    // 'https://anapioficeandfire.com/api/characters/585',
+    // 'https://anapioficeandfire.com/api/characters/586',
+    // 'https://anapioficeandfire.com/api/characters/587',
+];
+
 // main function that attaches button listener
 function main() {
     triggerElement.addEventListener('click', function () {
         // console.log('about to start the promise chain');
-        jokePromise = getJoke();
+        let arrayOfPromises = URLs.map(makeAjaxRequest);
+        console.log(arrayOfPromises);
+
+        Promise.all(arrayOfPromises)
+            .then(function (arrayOfResults) {
+                // Your .then waits for every promise 
+                // in the array to come back.
+                let chuck = arrayOfResults[0];
+                let snow = arrayOfResults[1];
+
+                // drawChuck(chuck);
+                // drawPoutyFace(snow);
+
+                // Your arrayOfResults 
+                console.log(arrayOfResults);
+                // return arrayOfResults.map(c => c.name);
+            })
+            // .then(function (arrayOfNames) {
+            //     // console.log(arrayOfNames);
+            //     arrayOfNames.forEach();
+            // })
+
+
+
+
         // console.log('ok, I started the promise chain');
         // console.log(jokePromise);
         // console.log('that was a promise for the joke');
