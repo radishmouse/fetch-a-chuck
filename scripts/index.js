bbc87c8a66e0c8942d30b44e9c7ef1031b72ed0a
Chris Aquino <chris.aquino@gmail.com>
2018-10-24 15:43:21 -0400

    adds comments for CORS

--- scripts/index.js
+++ scripts/index.js
@@ -2,79 +2,23 @@
 const triggerElement = document.querySelector('[ data-trigger]');
 const outputElement = document.querySelector('[ data-output]');
 
-// Global joke cache
-let cachedJoke = 'Why did chuck norris cross the road? The road knew better than to cross him.';
-let jokePromise;
-
-// function that gets a chuck norris joke
-function getJoke() {
-    return fetch('https://api.icndb.com/jokes/random')
-        .then(convertToJson) // this converts the thing to the thing
-        .then(cacheJoke)
-        .then(extractJokeText)
-        .catch(showCachedJoke)
-        .then(drawJoke)
-    // .then( j => {
-    // //   document.body.textContent = j.value.joke
-    //     drawJoke(j.value.joke);
-    // } )    
-}
-
-function showCachedJoke(err) {
-    console.log(err);
-    // drawJoke(cachedJoke);
-    return cachedJoke;
-}
-
-function convertToJson(r) {
-    console.log(r);
-    let payload = r.json();
-    // create intermediate variable for 
-    // debugging the response
-    console.log(payload);
-    return payload;
-    // what is convertToJson returning?
-}
-
-function cacheJoke(jokeObj) {
-    // save jokeJson 
-    // 1. global variable
-    cachedJoke = jokeObj.value.joke || cachedJoke; // keep what's in cachedJoke if jokeObj.value.joke is falsey
-    return jokeObj;
-    // 2. localStorage
-}
-
-function extractJokeText(jokeObj) {
-    // debugger;
-    // console.log(jokeObj);
-    return jokeObj.value.joke;
-    // return "Why did chuck norris cross the road? The road knew better than to cross him."
-}
-
-// function that draws joke to DOM
-function drawJoke(jokeText) {
-    const newJoke = document.createElement('li');
-    newJoke.textContent = jokeText;
-
-    // not returning, just appending
-    outputElement.appendChild(newJoke);
-}
-
 function handleZodiacError(whichSign) {
     console.log(whichSign);
 }
 
+// Fetches the zodiac information from the API.
+// The API sends back the Response, but does not include the "Access-Control-Allow-Origin"
+// Without that header, the browser thinks the contents of the Response aren't safe to use.
+// So, your JavaScript is not allowed to access it.
 function getZodiac() {
-
-    let sign = "aries";
-
+    // This only happens when you're accessing an API built by someone else (i.e., not you).
+    // The workaround is to use the "my-little-cors-proxy" that adds the appropriate header.
     fetch('https://my-little-cors-proxy.herokuapp.com/https://zodiacal.herokuapp.com/api')
         .then(convertToJson)
         .then(function (payload) {
-            //
-            // return sign;
+            console.log(payload);
+            // Only logging to console for now, for demo purposes
         })
-        .then(function () {})
         .catch(function () {
             handleZodiacError(sign);
         });
@@ -83,13 +27,6 @@ function getZodiac() {
 
 // main function that attaches button listener
 function main() {
-    triggerElement.addEventListener('click', function () {
-        console.log('about to start the promise chain');
-        // jokePromise = getJoke();
-        getZodiac();
-        console.log('ok, I started the promise chain');
-        // console.log(jokePromise);
-        // console.log('that was a promise for the joke');
-    });
+    triggerElement.addEventListener('click', getZodiac);
 }
 main();
